ca592562bd058ce46ee7152c91e7b11fb581b4e8
Nathan VandenHoek <natevandenhoek@gmail.com>
2021-12-30 15:07:07 -0500

    Update main.cpp spelling error.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 /********************************************************************************************
 * This main method will read the database.csv file, and seperate each line as a Superhero.
-* Then each superhero is stored using a hashing function, nad the number of collisions is
+* Then each superhero is stored using a hashing function, and the number of collisions is
 * recorded. The the number of collisions is printed out, and written to a RESULTS.md file.
 *
 * Using an sstream to get data form the file was found on StackOverflow.
